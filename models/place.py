04052b2e164ef6881aa38ee1c5019b2cce2d3f79
Dontcode44 <kabutto378@gmail.com>
2022-09-06 23:16:28 -0500

    Adds - Amenity and Places

--- models/place.py
+++ models/place.py
@@ -50,37 +50,29 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-        if getenv('HBNB_TYPE_STORAGE') != 'db':
+    def __init__(self, *args, **kwargs):
+            """initializes Place"""
+            super().__init__(*args, **kwargs)
 
-            @property
-            def reviews(self):
-                """Reviewis
-                """
-                my_dict = models.storage.all('Review')
-                my_list = []
-                for review in my_dict.values():
-                    if review.place_id == self.id:
-                        my_list.append(review)
-
-                return my_list
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
 
         @property
         def amenities(self):
-            """
-            returns the list of Amenity instances based on the
-            attribute amenity_ids
-            """
-            my_dict = models.storage.all('Amenity')
-            for amenity in my_dict.values():
+            """getter attribute returns the list of Amenity instances"""
+            from models.amenity import Amenity
+            amenity_list = []
+            all_amenities = models.storage.all(Amenity)
+            for amenity in all_amenities.values():
                 if amenity.place_id == self.id:
-                    amenities_ids.append(amenity)
-            return self.amenities_ids
-
-        @amenities.setter
-        def amenities(self, obj=None):
-            """
-            handles append method for adding an Amenity.id to the
-            attribute amenity_ids
-            """
-            if type(obj) == 'Amenity':
-                self.amenities_ids.append(obj.id)
+                    amenity_list.append(amenity)
+            return amenity_list
