e3a240eec283e64783ea069049c68f4ddc3d32f2
Dontcode44 <kabutto378@gmail.com>
2022-09-06 20:47:58 -0500

    Fixed - Code trash fork

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """New engine DBStorage"""
-from unicodedata import name
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
@@ -17,9 +16,7 @@ class DBStorage:
     """Private class attributes:"""
     __engine = None
     __session = None
-    classes = {"User": User, "State": State,
-               "City": City, "Amenity": Amenity,
-               "Place": Place, "Review": Review}
+    classes = ["User", "State", "City", "Amenity", "Place", "Review"]
 
     def __init__(self):
         """ Public instance methods:
@@ -43,7 +40,7 @@ class DBStorage:
         mydict = {}
         if cls is None:
             for myclass in self.classes:
-                result = self.__session.query(self.classes[myclass]).all()
+                result = self.__session.query(myclass).all()
                 for obj in result:
                     mydict["{}.{}".format(obj.__class__, obj.id)] = obj
         else:
