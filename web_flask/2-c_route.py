d46f59078307f5f5ac52ca289e52da8f3f5f92de
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-13 14:22:33 -0500

    [ADD]: Task 2 complete

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Flask application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """
+    Index entry for route /
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    Index entry for route /hbnb
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is_fun(text):
+    """
+    Index entry for route /hbnb
+    """
+    return "C {}".format(text.replace(" ", "_"))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
