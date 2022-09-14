8ac791b6276708649bc921c7ab4d7022094320c5
SebasGTX1 <sebas-0202@hotmail.es>
2022-09-13 22:20:14 -0500

    [ADD]: Task 7 complete

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Flask application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_session():
+    """ Teardown method"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list(response):
+    """ index for the route /states_list route """
+    states = storage.all(State)
+    return render_template('7-states_list.html',
+                           items=states.values())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
